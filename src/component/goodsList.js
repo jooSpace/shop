31f3cd0632a989144aade79a464ca6d27134f563
juhyung <juhyung@gmail.com>
2023-04-03 00:22:25 +0900

    first commit

---
+++ src/component/goodsList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const GoodsList = (props) => {
+
+    return (
+        <>              
+            {/* <img src={process.env.PUBLIC_URL + '/img.jpg'} width={300}></img> */}
+            <img src={'https://codingapple1.github.io/shop/shoes'+ (props.i+1) +'.jpg'} width={300} alt=""></img>
+            <h4>{props.shoes.title}</h4>
+            <p>{props.shoes.content}</p>
+            <p>{props.shoes.price}</p>
+        </>   
+    );
+}
+
+export default GoodsList;
---
+++ src/pages/main.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useState } from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+import data from '../data.js';
+import GoodsList from '../component/goodsList.js';
+
+const Main = () => {
+
+    let [shoes, setShoes] = useState(data);
+
+    return (
+        <div>
+            <div className='main-bg'>
+                
+            </div>
+            <Container>
+                <Row>
+                    {
+                    shoes.map((a, i) => {
+                            return (
+                                <Col key={shoes.id} sm={4}>
+                                    <GoodsList shoes={shoes[i]} i={i}></GoodsList>
+                                </Col> 
+                            )
+                        })  
+                    }
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default Main;
